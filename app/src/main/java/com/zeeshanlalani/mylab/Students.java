0861bccf01bcb41616e53c2c6a6b7f9d35d0fc19
Computer Lab Admin <admin@jinnah.edu>
2017-10-14 11:26:43 +0500

    list item click

--- app/src/main/java/com/zeeshanlalani/mylab/Students.java
+++ app/src/main/java/com/zeeshanlalani/mylab/Students.java
@@ -1,19 +1,39 @@
 package com.zeeshanlalani.mylab;
 
 import android.app.ListActivity;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class Students extends ListActivity {
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
 
-        String[] students = getResources().getStringArray(R.array.student_names);
-        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.lblStudentName, students));
+        final String[] list = {"0","0","0","0","0","0","0","0","0","0"};
+
+        this.setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, R.id.lblStudentName, list));
+
+        listView = getListView();
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+//                AlertDialog.Builder alert = new AlertDialog.Builder(Students.this);
+//                alert.setMessage(String.valueOf(id));
+//                alert.show();
+                String num = ((TextView)view).getText().toString();
+                int val = Integer.valueOf(num) + 1;
+                ((TextView)view).setText(String.valueOf(val));
+            }
+        });
 
     }
 }
